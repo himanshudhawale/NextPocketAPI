2b2c55a1ac796e73a2cedf048481fe5e8a72df70
himanshudhawale <himanshudhawale2@icloud.com>
2019-11-12 15:17:53 -0500

    port changed

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(errorHandler);
 
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80):5000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80):80;
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
     console.log('Address is: ' + server.address().address);
