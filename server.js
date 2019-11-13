5cd4f1575536541ac33240bd70f92cc60f1b4e83
himanshudhawale <himanshudhawale2@icloud.com>
2019-11-12 19:23:39 -0500

    s

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(errorHandler);
 
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80):80;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80):5000;
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
