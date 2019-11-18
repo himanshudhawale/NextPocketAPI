3c50bc957c6abac6d76db1c0ae46e035a9e1f14c
himanshudhawale <himanshudhawale2@icloud.com>
2019-11-18 18:21:59 -0500

    removed node modules

--- users/user.service.js
+++ users/user.service.js
@@ -6,11 +6,6 @@ const User = db.User;
 const teamdb = require('_helpers/teamdb')
 const Team = teamdb.Team;
 const braintree = require('braintree');
-// var Simplify = require("simplify-commerce"),
-//     client = Simplify.getClient({
-//         publicKey: 'sbpb_OGI3MjliOTUtYWMyZS00ZTE4LWFmYTgtNTdkZDQ1NTBhZGFm',
-//         privateKey: '6IpmiveAFIhKuVmp6CONqGsVnJKfuPc4C2q1endRa2l5YFFQL0ODSXAOkNtXTToq'
-//     });
 
 
 module.exports = {
